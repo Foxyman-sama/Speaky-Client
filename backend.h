e19aae8d6e172e0ce191c5f4abe21a1050b0b24e
Foxyman-sama <dimalatoho1448@gmail.com>
2024-11-25 12:41:25 +0200

    feat: update send and lazy connection

--- backend.h
+++ backend.h
@@ -12,20 +12,32 @@ class Backend : public QObject {
   explicit Backend(QObject* parent = nullptr) : QObject { parent } {}
 
   Q_INVOKABLE void send(QString room_id, QString name) {
-    boost::asio::io_context context;
-    boost::asio::ip::tcp::socket socket { context };
+    if (is_connected == false) {
+      connect();
+    }
+
+    const auto sent_data { room_id.toStdString() + ';' + name.toStdString() + '@' };
+    boost::asio::write(socket, boost::asio::buffer(sent_data));
+    qDebug() << "sent data: " << sent_data;
+
+    emit sendEmitted();
+  }
+
+ signals:
+  void sendEmitted();
+
+ private:
+  void connect() {
     boost::asio::ip::tcp::resolver::query resolver_query { "127.0.0.1", "9090",
                                                            boost::asio ::ip::tcp::resolver::query::numeric_service };
     boost::asio::ip::tcp::resolver resolver { context };
     auto it { resolver.resolve(resolver_query) };
-
     boost::asio::connect(socket, it);
-    qDebug() << room_id << ' ' << name;
-    emit printEmitted();
   }
 
- signals:
-  void printEmitted();
+  boost::asio::io_context context;
+  boost::asio::ip::tcp::socket socket { context };
+  bool is_connected { false };
 };
 
 #endif  // BACKEND_H
