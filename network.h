5e78859e6ec375667417c969f8373ee7b261aed9
Foxyman-sama <dimalatoho1448@gmail.com>
2024-11-26 14:21:29 +0200

    feat: finish with registration

--- network.h
+++ network.h
@@ -20,35 +20,15 @@ class Network : public QObject {
   Q_INVOKABLE void register_on_server(QString room_id, QString name) {
     connect();
 
-    RegisterPackage register_package { room_id.toStdString(), name.toStdString() };
-    const auto register_json { register_package.make_json() };
+    const RegistrationPackage registration_package { room_id.toStdString(), name.toStdString() };
+    write_into_socket(socket, registration_package);
 
-    std::ostringstream oss;
-    boost::property_tree::write_json(oss, register_json);
-    const auto json_str { oss.str() };
-    const auto json_size { json_str.size() };
-
-    InternetPackage internet_package;
-    std::memcpy(internet_package.get_data(), &json_size, internet_package.header_lentgh);
-    internet_package.reallocate();
-    std::memcpy(internet_package.get_body(), json_str.c_str(), internet_package.get_body_length());
-
-    boost::asio::write(socket,
-                       boost::asio::buffer(internet_package.get_data(),
-                                           internet_package.header_lentgh + internet_package.get_body_length()));
-
-    InternetPackage internet_package2;
-    boost::asio::read(socket, boost::asio::buffer(internet_package2.get_data(), internet_package2.header_lentgh));
-    internet_package2.reallocate();
-    boost::asio::read(socket, boost::asio::buffer(internet_package2.get_body(), internet_package2.get_body_length()));
-
-    boost::property_tree::ptree ptree;
-    std::istringstream iss { std::string { internet_package2.get_body() } };
-    boost::property_tree::read_json(iss, ptree);
-
-    StatusPackage status_package { ptree };
+    const auto status_package { read_from_socket<StatusPackage>(socket) };
     if (status_package.status == "OK") {
-      qDebug() << "Everything's OK";
+      const auto message_package { read_from_socket<MessagePackage>(socket) };
+      while (message_package.username.empty() == false) {
+        model.append(message_package);
+      }
     }
 
     emit register_on_serverEmitted();
